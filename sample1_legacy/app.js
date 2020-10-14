d6dc939c488c48c3bd2f7ab19c24b6bf79564e7b
rotoshine <rotoshine@gmail.com>
2020-10-15 04:21:05 +0900

    add sample1

---
+++ sample1_legacy/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const axios = require('axios')
+
+const app = express()
+
+const CMS_URL = 'https://admin.idiots.band'
+const PAGE_PER_COUNT = 6
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+app.use('/public', express.static('public'))
+
+app.get('/', async (req, res) => {
+  const { data: lives } = await axios.get(`${CMS_URL}/lives?_limit=${PAGE_PER_COUNT}`)
+  return res.render('live', {
+    layout: false,
+    lives: lives.map(live => ({
+      bands: live.bands,
+      title: live.title,
+      club: live.club,
+      date: live.date,
+      link: `https://idiots.band/live/${live.slug}/`,
+      posterUrl: `${CMS_URL}${live.posters[0].url}`,
+    })),
+  })
+})
+
+app.listen(3000)
