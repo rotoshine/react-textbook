64467ab62e340f0384cb69cf4392769101b6c371
rotoshine <rotoshine@gmail.com>
2020-10-23 02:47:02 +0900

    2주차 샘플코드1 추가

--- 1st/sample3_apply_react/src/Header.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-
-export default function Header() {
-  return <header>밴드 이디어츠 공연 일정</header>
-}
---
+++ 2nd/sample1/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Greeting from './Greeting'
+
+ReactDOM.render(<Greeting />, document.getElementById('root'))
